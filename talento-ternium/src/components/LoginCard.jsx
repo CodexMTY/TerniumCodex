259989bf389f9db778a3235804a281387dc71396
Joaquin Aguirre <a01177479@tec.mx>
2023-05-28 22:26:53 -0600

    Se arreglo el código y se agregregó el componente de AuthCard y la función de apiUtils

--- talento-ternium/src/components/LoginCard.jsx
+++ talento-ternium/src/components/LoginCard.jsx
@@ -1,100 +1,49 @@
 import React, { useState } from 'react';
-import { Card, Container, Form, Button } from 'react-bootstrap';
-import terniumLogo from '../img/logo-ternium.png';
+import { Form } from 'react-bootstrap';
 import { Link , useNavigate } from 'react-router-dom';
 import Cookies from 'universal-cookie';
-
-const API = 'https://codextern-4ny2.onrender.com/';
+import AuthCard from './AuthCard';
+import { postRequest } from '../apiUtils';
 
 function LoginCard({ switchCard }) {
-
-  const cookies = new Cookies;
+  const cookies = new Cookies();
   const navigate = useNavigate();
 
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
-  const handleChangeEmail = async (e) => {
-    setEmail(e.target.value)
-  };
-  const handleChangePassword = async (e) => {
-    setPassword(e.target.value)
-  };
-
   const userLogin = async (e) => {
     e.preventDefault();
-    let userData =  {
-      "email": email,
-      "password": password
+    const result = await postRequest('auth/login', { email, password });
+    
+    if (result.error){
+      setErrorMessage("Credenciales incorrectas");
     }
-
-    fetch(`${API}auth/login`, {
-      method: 'POST',
-      headers: {accept: 'application/json', 'content-type': 'application/json'},
-      body: JSON.stringify(userData)})
-    .then((response) => response.json())
-    .then((result) => {
-      if (result.error){
-        setErrorMessage("Credenciales incorrectas")
-      }
-      else if(result.token){
-        cookies.set('token', result.token, {
-          path: '/'
-        });
-        cookies.set('exp', result.exp, {
-          path: '/'
-        });
-        cookies.set('user_id', result.user_id, {
-          path: '/'
-        });
-        navigate('/homePage');
-      }
-    })
+    else if(result.token){
+      cookies.set('token', result.token, { path: '/' });
+      cookies.set('exp', result.exp, { path: '/' });
+      cookies.set('user_id', result.user_id, { path: '/' });
+      navigate('/homePage');
+    }
   }
 
   return (
-    <Container className="d-flex justify-content-center align-items-center">
-      <Card style={{ width: '25rem' }}>
-        <Card.Body>
-          <img src={terniumLogo} className="img-fluid" alt="Logo Ternium" />
-          <Card.Text className="text-muted text-center mt-2">Sistema para equipo de talento</Card.Text>
+    <AuthCard onSubmit={userLogin} switchCard={switchCard} primaryButtonText="Iniciar sesión" secondaryButtonText="Registrarse">
+      <Form.Group controlId="formBasicEmail">
+        <Form.Control type="email" placeholder="Correo electrónico" value={email} onChange={(e) => setEmail(e.target.value)} />
+      </Form.Group>
 
-          <Form.Group controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="Correo electrónico" value={email} onChange={handleChangeEmail}/>
-          </Form.Group>
+      <Form.Group controlId="formBasicPassword" className="mt-2">
+        <Form.Control type="password" placeholder="Contraseña" value={password} onChange={(e) => setPassword(e.target.value)} />
+        {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      </Form.Group>
 
-          <Form.Group controlId="formBasicPassword" className="mt-2" value={password} onChange={handleChangePassword}>
-            <Form.Control type="password" placeholder="Contraseña" />
-            {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
-          </Form.Group>
-
-          <div className="mt-2">
-              <Link to='/passwordReset'>Olvidé mi contraseña</Link>
-          </div>
-
-          <Button
-            onClick={userLogin}
-            type="submit"
-            className="mt-3 py-2 w-75"
-            variant='outline-danger'
-          >
-            Iniciar sesión
-          </Button>
-
-
-          <Button
-            type="button"
-            className="mt-3 py-2 w-75"
-            variant='outline-danger'
-            onClick={switchCard}
-          >
-            Registrarse
-          </Button>
-        </Card.Body>
-      </Card>
-    </Container>
+      <div className="mt-2">
+        <Link to='/passwordReset'>Olvidé mi contraseña</Link>
+      </div>
+    </AuthCard>
   );
 }
 
-export default LoginCard
+export default LoginCard;
