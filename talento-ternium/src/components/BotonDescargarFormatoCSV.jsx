a82fa2de0494fdf4a92101b2b877dd0eb9af672d
dmtz01 <hdaniel.mtz01@gmail.com>
2023-06-14 19:28:01 -0600

    Se agrego la funcionalidad de batch upload para Upward Feedback y Cliente Proveedor. Se cambio el manejo de la carga de archivos a un solo pop-up.

--- talento-ternium/src/components/BotonDescargarFormatoCSV.jsx
+++ talento-ternium/src/components/BotonDescargarFormatoCSV.jsx
@@ -4,7 +4,7 @@ import { Nav, Button } from "react-bootstrap";
 
 const descargarArchivo = (contenido, nombreArchivo) => {
     const element = document.createElement("a");
-    const file = new Blob([contenido], { type: 'text/csv;charset=utf-8;' });
+    const file = new Blob([contenido], { type: "text/csv;charset=utf-8;" });
     element.href = URL.createObjectURL(file);
     element.download = nombreArchivo;
 
@@ -20,10 +20,13 @@ function BotonDescargarFormatoCSV() {
     const manejarAbrir = () => activarMostrar(true);
 
     const contenidoArchivoDatosEmpleado = `nombre,apellidos,email,idm4,cumpleanos,fecha_ingreso,universidad,direccion,puesto,pc_cat,resumen,key_talent,encuadre,cet,estructura3,estructura4,estructura5,jefe
-Nombre,Apellido,correodelempleado@ternium.com.mx,1,28/2/2002,30/3/2020,Universidad,Direccion,Puesto,1,Resumen,True/False,Encuadre,1,Estructura3,Estructura4,Estructura5,Jefe`;
+Nombre(evitar caracteres especiales para que no marque error),Apellido(evitar caracteres especiales para que no marque error),correodelempleado@ternium.com.mx,1,28/2/2002(dia/mes/anio),30/3/2020(dia/mes/anio),Universidad,Direccion,Puesto,1,Resumen,Escribir true o false,Encuadre,1,Estructura3,Estructura4,Estructura5,Jefe`;
+
+    const contenidoArchivoUpwardFClienteP = `email,promedio,comentarios
+correodelempleado@ternium.com.mx,Numero del 0 al 5(la base de datos utiliza promedio para el nombre de la variable pero es el puntaje),comentarios`
 
     const contenidoArchivoEvaluacionesAnuales = `email,ano,performance,potencial,curva
-correodelempleado@ternium.com.mx,2020,5,Potencial,Curva`
+correodelempleado@ternium.com.mx,2020,Numero del 0 al 5,Potencial,Curva`
 
     return (
       <>
@@ -38,18 +41,27 @@ correodelempleado@ternium.com.mx,2020,5,Potencial,Curva`
           <Modal.Body> 
             <Button
                 type="submit"
-                className="mt-2 py-2 w-30"
+                className="mt-2"
                 variant="outline-danger"
-                style={{ width: "100%" }}
+                style={{ width: "100%", height: "40px", fontSize: "14px" }}
                 onClick={() => descargarArchivo(contenidoArchivoDatosEmpleado, "FormatoDatosEmpleados.csv")}
             > 
                 Descargar formato para datos de empleados
             </Button>
             <Button
                 type="submit"
-                className="mt-3 mb-2 py-2 w-30"
+                className="mt-3"
                 variant="outline-danger"
-                style={{ width: "100%" }}
+                style={{ width: "100%", height: "40px", fontSize: "14px" }}
+                onClick={() => descargarArchivo(contenidoArchivoUpwardFClienteP, "FormatoUpwardFeedback_ClienteProveedor.csv")}
+            > 
+                Descargar formato para Upward Feedback y Cliente Proveedor
+            </Button>
+            <Button
+                type="submit"
+                className="mt-3 mb-2"
+                variant="outline-danger"
+                style={{ width: "100%", height: "40px", fontSize: "14px" }}
                 onClick={() => descargarArchivo(contenidoArchivoEvaluacionesAnuales, "FormatoEvaluacionesAnuales.csv")}
             >
                 Descargar formato para Evaluaciones Anuales
